3be8429515fe966391c2241c34be67b9a9b02e22
Arthurwang8 <148507250+Arthurwang8@users.noreply.github.com>
2023-11-08 20:31:28 -0800

    Update countEvens.cpp

--- countEvens.cpp
+++ countEvens.cpp
@@ -3,5 +3,11 @@
 #include <iostream>
 
 int countEvens(int a[], int size) {
-  return -42; // STUB!  Replace with correct code.
+  int count = 0;
+  for (int i = 0; i< a.size(); i++){
+  if(a[i]%2==0){
+  count ++;
+  }
+  }
+  return count; // STUB!  Replace with correct code.
 }
