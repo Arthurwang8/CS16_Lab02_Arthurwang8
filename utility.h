919fec1fb3eb078d7e3b5884ca09852f29d2e6b9
Arthurwang8 <148507250+Arthurwang8@users.noreply.github.com>
2023-11-08 20:40:24 -0800

    Update utility.h

--- utility.h
+++ utility.h
@@ -10,33 +10,6 @@
 // then be sure to  #include "utility.h" in the file where you use
 // these functions
 
-bool isOdd(int x) { 
-  if(x%2 == 1){
-    return true;
-  }
-   else{
-     return false;  
-   }// REPLACE THIS STUB WITH REAL CODE
-}
-bool isEven(int x) { 
-  if(x%2 == 0){
-    return true;
-  }
-   else{
-     return false;  
-   }
-}
-bool isPrime(int x) { 
-if(x <= 2){
-  return false;}
-if(x % 2 == 0) 
-{return false;
-                    }
-for(int i=3; i<=x; i++){
-if(x % i == 0 ) 
-{return false;
-}
-
-}
-  return true;
-}
+bool isOdd(int x);
+bool isEven(int x);
+bool isPrime(int x);
